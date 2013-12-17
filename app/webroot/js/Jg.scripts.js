3df273c641422c27a00bdf8b3dd54cd9d3957adc
Jorge Luis Gomez <jgomezv@outlook.com>
2013-12-17 09:13:26 -0500

    Insert Google analitics

--- app/webroot/js/Jg.scripts.js
+++ app/webroot/js/Jg.scripts.js
@@ -29,3 +29,13 @@ function in_array (needle, haystack, argStrict) {
 	}
 	return false;
 }
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-46449657-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
